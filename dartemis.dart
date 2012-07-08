e4b7521c5713ecd6fca22b5c1581ddbef2eef9cb
Dennis Kaselow <Dennis.Kaselow+github@gmail.com>
2012-07-08 16:00:03 +0200

    renamed fast_lookup_table to trigometry_lookup_table

--- dartemis.dart
+++ dartemis.dart
@@ -3,10 +3,10 @@
 #import('dart:coreimpl');
 
 #source('dartemis/utils/bag.dart');
-#source('dartemis/utils/fast_lookup_table.dart');
 #source('dartemis/utils/fast_math.dart');
 #source('dartemis/utils/immutable_bag.dart');
 #source('dartemis/utils/timer.dart');
+#source('dartemis/utils/trigometry_lookup_table.dart');
 #source('dartemis/utils/utils.dart');
 
 
