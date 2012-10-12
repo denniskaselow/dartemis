24280127c0c5950be78dc7e94faea8ca79f91fde
Dennis Kaselow <Dennis.Kaselow+github@gmail.com>
2012-10-12 18:40:39 +0200

    added pubspec and changed directory layout accordingly

---
+++ lib/dartemis.dart
@@ -0,0 +1,35 @@
+library dartemis;
+
+import 'dart:coreimpl';
+import 'dart:math';
+
+part 'src/utils/bag.dart';
+part 'src/utils/fast_math.dart';
+part 'src/utils/immutable_bag.dart';
+part 'src/utils/timer.dart';
+part 'src/utils/trigometry_lookup_table.dart';
+part 'src/utils/utils.dart';
+
+part 'src/component.dart';
+part 'src/component_mapper.dart';
+part 'src/component_type.dart';
+part 'src/component_type_manager.dart';
+
+part 'src/entity.dart';
+part 'src/entity_manager.dart';
+
+part 'src/entity_system.dart';
+part 'src/entity_processing_system.dart';
+part 'src/delayed_entity_system.dart';
+part 'src/delayed_entity_processing_system.dart';
+part 'src/interval_entity_system.dart';
+part 'src/interval_entity_processing_system.dart';
+
+part 'src/manager.dart';
+
+part 'src/system_manager.dart';
+part 'src/system_bit_manager.dart';
+part 'src/tag_manager.dart';
+part 'src/group_manager.dart';
+
+part 'src/world.dart';
