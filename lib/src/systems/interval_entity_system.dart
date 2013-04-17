edefc4f8d435863bf0fa48c9e8eb5c2f49789eb5
Dennis Kaselow <Dennis.Kaselow+github@gmail.com>
2013-04-17 19:23:53 +0200

    IntervalEntitySystem now provides access to the delta since the last time it has been invoked

--- lib/src/systems/interval_entity_system.dart
+++ lib/src/systems/interval_entity_system.dart
@@ -6,17 +6,31 @@ part of dartemis;
  */
 abstract class IntervalEntitySystem extends EntitySystem {
   num _acc = 0;
+  num _delta = 0;
   final num _interval;
+
+  /// Returns the accumulated delta since the system was last invoked.
+  num get delta => _delta;
 
   IntervalEntitySystem(this._interval, Aspect aspect) : super(aspect);
 
   bool checkProcessing() {
     _acc += world.delta;
+    _delta += world.delta;
     if(_acc >= _interval) {
       _acc -= _interval;
       return true;
     }
     return false;
+  }
+
+  /**
+   * Resets the accumulated delta to 0.
+   *
+   * Call `super.end()` if you overwrite this function.
+   */
+  void end() {
+    _delta = 0;
   }
 
 }
--- test/dartemis_test.dart
+++ test/dartemis_test.dart
@@ -485,6 +485,23 @@ main() {
     });
 
   });
+  group('IntervalEntitySystem tests', () {
+    test('delta returns accumulated time since last processing', () {
+      World world = new World();
+      var sut = new TestIntervalEntitySystem(40);
+      world.addSystem(sut);
+      world.delta = 16;
+
+      expect(sut.checkProcessing(), equals(false));
+      expect(sut.checkProcessing(), equals(false));
+      expect(sut.checkProcessing(), equals(true));
+      expect(sut.delta, equals(48));
+      sut.end();
+      expect(sut.checkProcessing(), equals(false));
+      expect(sut.checkProcessing(), equals(true));
+      expect(sut.delta, equals(32));
+    });
+  });
 }
 
 typedef void EntitySystemStarter(EntitySystem es);
@@ -526,4 +543,8 @@ class TestEntitySystem extends EntitySystem {
   }
 
   bool checkProcessing() => true;
+}
+
+class TestIntervalEntitySystem extends IntervalEntitySystem {
+  TestIntervalEntitySystem(num interval) : super(interval, Aspect.getEmpty());
 }
