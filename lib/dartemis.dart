387276e788a280f4a5bdea6ac03d9ad7f177fca7
Dennis Kaselow <dennis.kaselow+git@gmail.com>
2022-09-03 13:56:38 +0200

    meta package is being used for experimental annoation

--- lib/dartemis.dart
+++ lib/dartemis.dart
@@ -7,35 +7,27 @@ import 'package:meta/meta.dart';
 
 import 'src/core/utils/bit_set.dart';
 
-part 'src/core/utils/bag.dart';
-part 'src/core/utils/entity_bag.dart';
-part 'src/core/utils/object_pool.dart';
-
+part 'src/core/aspect.dart';
 part 'src/core/component.dart';
-part 'src/core/mapper.dart';
 part 'src/core/component_manager.dart';
 part 'src/core/component_type.dart';
-
-part 'src/core/aspect.dart';
-
 part 'src/core/entity_manager.dart';
 part 'src/core/entity_observer.dart';
-
 part 'src/core/entity_system.dart';
-part 'src/core/systems/entity_processing_system.dart';
-part 'src/core/systems/interval_entity_system.dart';
-part 'src/core/systems/interval_entity_processing_system.dart';
-part 'src/core/systems/void_entity_system.dart';
-
 part 'src/core/manager.dart';
-
-part 'src/core/system_bit_manager.dart';
-
 part 'src/core/managers/group_manager.dart';
 part 'src/core/managers/player_manager.dart';
 part 'src/core/managers/tag_manager.dart';
 part 'src/core/managers/team_manager.dart';
-
+part 'src/core/mapper.dart';
+part 'src/core/system_bit_manager.dart';
+part 'src/core/systems/entity_processing_system.dart';
+part 'src/core/systems/interval_entity_processing_system.dart';
+part 'src/core/systems/interval_entity_system.dart';
+part 'src/core/systems/void_entity_system.dart';
+part 'src/core/utils/bag.dart';
+part 'src/core/utils/entity_bag.dart';
+part 'src/core/utils/object_pool.dart';
 part 'src/core/world.dart';
 
 part 'src/metadata/generate.dart';
